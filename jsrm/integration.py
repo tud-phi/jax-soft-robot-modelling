ff13262ff88079ecb3adcf229459042e6a0f7af7
Maximilian Stölzle <maximilian@stoelzle.ch>
2023-02-22 17:55:28 +0100

    Unfortunately, we cannot jit the `ode_factory` function

--- jsrm/integration.py
+++ jsrm/integration.py
@@ -3,27 +3,54 @@ from jax import config as jax_config
 
 jax_config.update("jax_enable_x64", True)  # double precision
 import jax
-from jax import Array, jit, vmap
+from jax import Array, debug, jit, vmap
 from jax import numpy as jnp
+from jax.tree_util import Partial
 from typing import Callable, Dict
 
 from jsrm.systems import euler_lagrangian
 
 
-@jit
-def make_ode(
+def ode_factory(
         dynamical_matrices_fn: Callable,
         params: Dict[str, Array],
         tau: Array
 ) -> Callable[[float, Array], Array]:
-    nonlinear_state_space_fn = partial(
-        euler_lagrangian.nonlinear_state_space,
-        dynamical_matrices_fn,
-        params,
-        tau=tau
-    )
-
-    def ode_fn(t: float, x: Array):
-        return nonlinear_state_space_fn(x)
+    """
+    Make an ODE function of the form ode_fn(t, x) -> x_dot.
+    This function assumes a constant torque input (i.e. zero-order hold).
+    Args:
+        dynamical_matrices_fn: Callable that returns the B, C, G, K, D, and A matrices. Needs to conform to the signature:
+            dynamical_matrices_fn(params, q, q_d) -> Tuple[B, C, G, K, D, A]
+            where q and q_d are the configuration and velocity vectors, respectively,
+            B is the inertia matrix of shape (n_q, n_q),
+            C is the Coriolis matrix of shape (n_q, n_q),
+            G is the gravity vector of shape (n_q, ),
+            K is the stiffness vector of shape (n_q, ),
+            D is the damping matrix of shape (n_q, n_q),
+            and A is the actuation matrix of shape (n_q, n_tau).
+        params: Dictionary with robot parameters
+        tau: torque vector of shape (n_tau, )
+    Returns:
+        ode_fn: ODE function of the form ode_fn(t, x) -> x_dot
+    """
+    @jit
+    def ode_fn(t: float, x: Array, *args) -> Array:
+        """
+        ODE of the dynamical Lagrangian system.
+        Args:
+            t: time
+            x: state vector of shape (2 * n_q, )
+            args: additional arguments
+        Returns:
+            x_d: time-derivative of the state vector of shape (2 * n_q, )
+        """
+        x_d = euler_lagrangian.nonlinear_state_space(
+            dynamical_matrices_fn,
+            params,
+            x,
+            tau,
+        )
+        return x_d
 
     return ode_fn
