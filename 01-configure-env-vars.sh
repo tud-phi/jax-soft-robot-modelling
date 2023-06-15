9fd513946b7261259748bc70c4946167191635bc
Maximilian Stolzle <maximilian@stoelzle.ch>
2023-06-15 13:50:28 +0200

    Add script for sourcing environment variables

---
+++ 01-configure-env-vars.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Run the following commands to
+# sudo find / -name 'nvcc'  # Path to binaries
+# sudo find / -name 'libcublas.so.*'  # Path to libraries
+
+# disable pre-allocation of memory for JAX
+export XLA_PYTHON_CLIENT_PREALLOCATE=false
+
+# add the src folder to the PYTHONPATH
+export PYTHONPATH="${PYTHONPATH}://jsrm"
