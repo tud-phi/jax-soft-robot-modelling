c7a841a7a2092c6b813f53ee5aba727b8b16ec8e
Maximilian Stolzle <maximilian@stoelzle.ch>
2023-07-04 17:49:24 +0200

    Add a test for the HSA end-effector kinematics

--- tests/test_planar_hsa.py
+++ tests/test_planar_hsa.py
@@ -76,7 +76,48 @@ params = {
 }
 
 
+def test_end_effector_kinematics(seed: int = 0):
+    print("Testing end effector kinematics...")
+    (
+        _,
+        forward_kinematics_end_effector_fn,
+        jacobian_end_effector_fn,
+        inverse_kinematics_end_effector_fn,
+        _,
+        sys_helpers,
+    ) = planar_hsa.factory(sym_exp_filepath)
+
+    # load saved symbolic data
+    sym_exps = dill.load(open(str(sym_exp_filepath), "rb"))
+
+    rng = random.PRNGKey(seed)
+    for idx in range(10):
+        rng, subrng1, subrng2, subrng3, subrng4, subrng5 = random.split(rng, 6)
+        kappa_b = random.uniform(
+            subrng1,
+            (num_segments,),
+            minval=-jnp.pi / jnp.mean(params["l"]),
+            maxval=jnp.pi / jnp.mean(params["l"]),
+        )
+        sigma_sh = random.uniform(subrng2, (num_segments,), minval=-0.2, maxval=0.2)
+        sigma_a = random.uniform(subrng3, (num_segments,), minval=0.0, maxval=0.5)
+        q = jnp.concatenate((kappa_b, sigma_sh, sigma_a), axis=0)
+
+        print("q = ", q)
+
+        # forward kinematics
+        chiee = forward_kinematics_end_effector_fn(params, q)
+        # inverse kinematics
+        q_rec = inverse_kinematics_end_effector_fn(params, chiee)
+
+        if not jnp.allclose(q, q_rec, atol=1e-6):
+            print("q = ", q)
+            print("q_rec = ", q_rec)
+            raise ValueError("q != q_rec")
+
+
 def test_symbolic_and_numeric_implementation(seed: int = 0):
+    print("Testing symbolic and numeric implementation...")
     (
         forward_kinematics_virtual_backbone_fn,
         forward_kinematics_end_effector_fn,
@@ -88,9 +129,6 @@ def test_symbolic_and_numeric_implementation(seed: int = 0):
 
     # load saved symbolic data
     sym_exps = dill.load(open(str(sym_exp_filepath), "rb"))
-
-    B_xi = sys_helpers["B_xi"]
-    xi_eq = sys_helpers["xi_eq"]
 
     exps_subs = substitute_params_into_all_symbolic_expressions(sym_exps, params)
     state_syms = sym_exps["state_syms"]
@@ -110,7 +148,7 @@ def test_symbolic_and_numeric_implementation(seed: int = 0):
     @jit
     def matrices_sym(q: Array, phi: Array) -> Tuple[Array, Array, Array]:
         # map the configuration to the strains
-        xi = xi_eq + B_xi @ q
+        xi = sys_helpers["configuration_to_strains_fn"](params, q)
 
         # add a small number to the bending strain to avoid singularities
         xi_epsed = sys_helpers["apply_eps_to_bend_strains_fn"](xi, 1e-2)
@@ -157,4 +195,5 @@ def test_symbolic_and_numeric_implementation(seed: int = 0):
 
 
 if __name__ == "__main__":
+    test_end_effector_kinematics()
     test_symbolic_and_numeric_implementation()
