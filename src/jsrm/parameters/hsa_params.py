8a1f2259847f636222fe943bd1435fb1c4296a74
Maximilian Stolzle <maximilian@stoelzle.ch>
2023-10-20 18:10:00 +0200

    Decrease damping of EPU material to reduce time lag

--- src/jsrm/parameters/hsa_params.py
+++ src/jsrm/parameters/hsa_params.py
@@ -200,11 +200,11 @@ def generate_base_params_for_epu(num_segments: int = 1, num_rods_per_segment: in
         # Scaling of axial stiffness with twist strain [Nm/rad]
         "C_S_a": 0.00981059 * ones_rod,
         # damping coefficient for bending of shape (num_segments, rods_per_segment)
-        "zetab": 3e-5 * ones_rod,
+        "zetab": 4e-6 * ones_rod,
         # damping coefficient for shear of shape (num_segments, rods_per_segment)
-        "zetash": 2e-4 * ones_rod,
+        "zetash": 8e-5 * ones_rod,
         # damping coefficient for axial elongation of shape (num_segments, rods_per_segment)
-        "zetaa": 2e-2 * ones_rod,
+        "zetaa": 5e-3 * ones_rod,
         # maximum twist angles (positive) [rad]
         "phi_max": 270 / 180 * jnp.pi * ones_rod,
     }
